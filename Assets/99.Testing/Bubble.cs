737a625c5714c32e739a2947486c1d375857b9d0
JORGE PEDRERO <jorgepedreror1@hotmail.com>
2025-01-25 09:05:22 +0900

    Added base bubble class

---
+++ Assets/99.Testing/Bubble.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    /// <summary>
+    /// Called to map an integer value to this bubble’s visual representation.
+    /// </summary>
+    public virtual void mapBubble(int x)
+    {
+        
+        // Child classes can override this with  visuals.
+        Debug.Log($"Bubble mapped to value {x}");
+    }
+}
