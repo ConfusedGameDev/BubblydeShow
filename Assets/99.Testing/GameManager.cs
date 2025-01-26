b6867e3a0521b6ecef8d8428a121555b8ebe4b14
Pedreror1 <jorgepedreror1@hotmail.com>
2025-01-26 13:56:38 +0900

    dev- gameover setup

--- Assets/99.Testing/GameManager.cs
+++ Assets/99.Testing/GameManager.cs
@@ -45,11 +45,11 @@ public class GameManager : MonoBehaviour
     public List<CatRigController> cats;
     public GameObject catStep;
 
-    public AudioClip leftClip, rightClip, correctClip,wrongClip;
-    public AudioSource source;
+    public AudioClip leftClip, rightClip, correctClip, wrongClip, clearClip;
+    public AudioSource fxSource, bgmSource;
 
-    public GameObject  catDissapearTL;
-    bool gameStarted=false;
+    public GameObject catDissapearTL;
+    bool gameStarted = false;
     public TableReference tableReference;
     public TableEntryReference winReference, looseReference, questionReference, bubblyReference;
 
@@ -58,10 +58,15 @@ public class GameManager : MonoBehaviour
     public List<TMPro.TextMeshProUGUI> labels;
 
     public int valuesOffset = 2;
+
+    public float leftOffset = -500, rightOffset = 140.3f;
+    public float stepsSpacing = 2.25f;
+
+    public GameObject replayIcon, gameOverLabel, gameClearLabel, gameOverVideo, gameDescriptorLabel;
     void setFont(string locale)
     {
         labels = FindObjectsByType<TMPro.TextMeshProUGUI>(FindObjectsSortMode.None).ToList();
-        switch(locale)
+        switch (locale)
         {
             case "Japanese (ja)":
                 foreach (var label in labels)
@@ -96,15 +101,15 @@ public class GameManager : MonoBehaviour
     {
         InitializeGame();
         timer = 3f;
-       // setFont(LocalizationSettings.SelectedLocale.LocaleName);
+        // setFont(LocalizationSettings.SelectedLocale.LocaleName);
 
         while (timer > 0.1f)
         {
             //label.text = LocalizationSettings.StringDatabase.GetLocalizedString(tableReference, winReference);
-              
+
             timer -= Time.deltaTime;
             timerUI.fillAmount = 0;
-            timerLabel.text = Mathf.CeilToInt(timer).ToString() ;
+            timerLabel.text = Mathf.CeilToInt(timer).ToString();
             yield return null;
         }
         yield return new WaitForEndOfFrame();
@@ -112,6 +117,9 @@ public class GameManager : MonoBehaviour
         gameStarted = true;
         timer = timerDuration;
         ShowComparisonBubbles();
+
+        if (gameDescriptorLabel)
+            gameDescriptorLabel.SetActive(true);
 
     }
 
@@ -123,40 +131,37 @@ public class GameManager : MonoBehaviour
         }
         timer -= Time.deltaTime;
         timerUI.fillAmount = timer / timerDuration;
-        if(timerLabel)
-            timerLabel.text= Mathf.RoundToInt( timer).ToString();
+        if (timerLabel)
+            timerLabel.text = Mathf.RoundToInt(timer).ToString();
         if (timer <= 0)
         {
             GameOver();
         }
 
         //delete
-        for (int i = 0; i < cats.Count; i++)
-        {
-            cats[i].transform.position = transform.position - (Vector3.right * 14f) + (Vector3.fwd * 15f) - Vector3.up * 7.5f + Vector3.right * i * 2.25f + stepOffset;
-        }
          
+
     }
 
     void InitializeGame()
     {
         bubbles = Enumerable.Range(1, numberOfValues).ToList();
-        
+
         bubbles = bubbles.OrderBy(x => rnd.Next()).ToList();
         sortedItems = 0;
         currentIndex = 0;
         timer = timerDuration;
         ResetUnusedList();
         GetNext();
-        
+
         for (int i = 0; i < bubbles.Count; i++)
         {
-            originalBubbles.Add( bubbles[i]);
-            var startpos = transform.position -  (Vector3.right * 14f) + (Vector3.fwd * 15f) - Vector3.up * 7.5f + Vector3.right * i * 2.25f+stepOffset;           
-            var newCat= Instantiate(catStep,startpos, Quaternion.identity).GetComponent<CatRigController>();
-            newCat.initialPos = startpos ;
-            newCat.rootOffset= Vector3.up*bubbles[i];
-            newCat.offset= bubbles[i];
+            originalBubbles.Add(bubbles[i]);
+            var startpos = transform.position - (Vector3.right * 14f) + (Vector3.fwd * 15f) - Vector3.up * 7.5f + Vector3.right * i * stepsSpacing + stepOffset;
+            var newCat = Instantiate(catStep, startpos, Quaternion.identity).GetComponent<CatRigController>();
+            newCat.initialPos = startpos;
+            newCat.rootOffset = Vector3.up * bubbles[i];
+            newCat.offset = bubbles[i];
             cats.Add(newCat);
         }
     }
@@ -190,14 +195,14 @@ public class GameManager : MonoBehaviour
         int rnd = UnityEngine.Random.Range(0, bubblePrefabs.Count);
         currentBubbleGO = Instantiate(bubblePrefabs[rnd], leftPos, Quaternion.identity);
         var currentBubbleScript = currentBubbleGO.GetComponent<Bubble>();
-        currentBubbleScript.mapBubble(bubbles[currentIndex]*valuesOffset);
-        currentBubbleScript.offsetParent(Vector3.right* -500f);
+        currentBubbleScript.mapBubble(bubbles[currentIndex] * valuesOffset);
+        currentBubbleScript.offsetParent(Vector3.right * leftOffset);
         currentBubbleScript.upScaleParent(1.2f);
         nextBubbleGO = Instantiate(bubblePrefabs[rnd], rightPos, Quaternion.identity);
         var nextBubbleScript = nextBubbleGO.GetComponent<Bubble>();
-        nextBubbleScript.mapBubble(bubbles[nextIndex]*valuesOffset);
+        nextBubbleScript.mapBubble(bubbles[nextIndex] * valuesOffset);
         label.text = currentBubbleScript.mondai;
-        nextBubbleScript.offsetParent(Vector3.right * 140.3f);
+        nextBubbleScript.offsetParent(Vector3.right * rightOffset);
         nextBubbleScript.upScaleParent(1.2f);
     }
 
@@ -210,10 +215,10 @@ public class GameManager : MonoBehaviour
         if (isGameDone) return;
         if (bubbles[currentIndex] >= bubbles[nextIndex])
         {
-            if (source && correctClip)
+            if (fxSource && correctClip)
             {
-                source.Stop();
-                source.PlayOneShot(correctClip);
+                fxSource.Stop();
+                fxSource.PlayOneShot(correctClip);
             }
             int temp = bubbles[nextIndex];
             bubbles[nextIndex] = bubbles[currentIndex];
@@ -224,13 +229,14 @@ public class GameManager : MonoBehaviour
                 sortedItems++;
                 if (sortedItems >= bubbles.Count)
                 {
+
                     Win();
                     return;
                 }
                 ResetUnusedList();
                 currentIndex = 0;
             }
-            StartCoroutine(animateSteps(0.5f,bubbles));
+            StartCoroutine(animateSteps(0.5f, bubbles));
             if (hasWon())
             {
                 Win();
@@ -247,10 +253,10 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            if (source && wrongClip)
+            if (fxSource && wrongClip)
             {
-                source.Stop();
-                source.PlayOneShot(wrongClip);
+                fxSource.Stop();
+                fxSource.PlayOneShot(wrongClip);
             }
             GameOver();
         }
@@ -258,7 +264,7 @@ public class GameManager : MonoBehaviour
 
     public bool hasWon()
     {
-        for (int x = 0;x < bubbles.Count-1;x++)
+        for (int x = 0; x < bubbles.Count - 1; x++)
         {
             if (bubbles[x] > bubbles[x + 1])
                 return false;
@@ -274,10 +280,10 @@ public class GameManager : MonoBehaviour
         if (isGameDone) return;
         if (bubbles[currentIndex] <= bubbles[nextIndex])
         {
-            if(source && correctClip)
+            if (fxSource && correctClip)
             {
-                source.Stop();
-                source.PlayOneShot(correctClip);
+                fxSource.Stop();
+                fxSource.PlayOneShot(correctClip);
             }
             currentIndex = nextIndex;
             if (currentIndex == bubbles.Count - sortedItems - 1)
@@ -291,7 +297,7 @@ public class GameManager : MonoBehaviour
                 ResetUnusedList();
                 currentIndex = 0;
             }
-            StartCoroutine(animateSteps(0.5f,bubbles));
+            StartCoroutine(animateSteps(0.5f, bubbles));
             if (hasWon())
             {
                 Win();
@@ -307,10 +313,10 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            if (source && wrongClip)
+            if (fxSource && wrongClip)
             {
-                source.Stop();
-                source.PlayOneShot(wrongClip);
+                fxSource.Stop();
+                fxSource.PlayOneShot(wrongClip);
             }
             GameOver();
         }
@@ -318,7 +324,12 @@ public class GameManager : MonoBehaviour
 
     public IEnumerator AnimateBubblesort()
     {
-        for (int j   = 0; j < originalBubbles.Count; j++)
+        if (fxSource && clearClip)
+        {
+            fxSource.PlayOneShot(clearClip);
+        }
+
+        for (int j = 0; j < originalBubbles.Count; j++)
         {
             for (int i = 0; i < originalBubbles.Count - 1; i++)
             {
@@ -327,22 +338,22 @@ public class GameManager : MonoBehaviour
                     var tmp = originalBubbles[i + 1];
                     originalBubbles[i + 1] = originalBubbles[i];
                     originalBubbles[i] = tmp;
-                    yield return animateSteps(0.35f,originalBubbles);
+                    yield return animateSteps(0.15f, originalBubbles);
                 }
- 
+
 
             }
-         }
+        }
 
-        
+
 
 
     }
     public IEnumerator animateSteps(float duration, List<int> data)
     {
         var delta = 0f;
-        List<float> originalOffsets= new List<float>();
-        List<Vector3> originalRootOffsets= new List<Vector3>();
+        List<float> originalOffsets = new List<float>();
+        List<Vector3> originalRootOffsets = new List<Vector3>();
         for (int x = 0; x < cats.Count; x++)
         {
             originalOffsets.Add(cats[x].offset);
@@ -350,13 +361,13 @@ public class GameManager : MonoBehaviour
         }
         while (delta < duration)
         {
-            
+
             for (int x = 0; x < cats.Count; x++)
             {
                 // steps[x].transform.localScale = Vector3.Lerp(steps[x].transform.localScale, Vector3.one + Vector3.up * bubbles[x],Time.deltaTime);
-                var newCat= cats[x];
-                newCat.rootOffset = Vector3.Lerp(originalRootOffsets[x], Vector3.up * data[x],delta/duration);
-                newCat.offset = Mathf.Lerp(originalOffsets[x], data[x], delta/duration );
+                var newCat = cats[x];
+                newCat.rootOffset = Vector3.Lerp(originalRootOffsets[x], Vector3.up * data[x], delta / duration);
+                newCat.offset = Mathf.Lerp(originalOffsets[x], data[x], delta / duration);
                 delta += Time.deltaTime;
                 yield return new WaitForEndOfFrame();
             }
@@ -365,11 +376,11 @@ public class GameManager : MonoBehaviour
 
     public IEnumerator animateStepsEnding(int index)
     {
-        var duration =   UnityEngine.Random.Range(1f, 4.5f);
+        var duration = UnityEngine.Random.Range(0.5f, 1f);
         var delta = 0f;
-        float originalOffset= cats[index].offset;
-          
-         
+        float originalOffset = cats[index].offset;
+
+
         while (delta < duration)
         {
 
@@ -378,10 +389,10 @@ public class GameManager : MonoBehaviour
             delta += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
-        if(catDissapearTL)
+        if (catDissapearTL)
         {
             Instantiate(catDissapearTL, cats[index].rootBone.position, Quaternion.identity);
-                        cats[index].gameObject.SetActive(false);
+            cats[index].gameObject.SetActive(false);
 
         }
     }
@@ -396,12 +407,30 @@ public class GameManager : MonoBehaviour
 
     void GetNext()
     {
-        nextIndex= currentIndex + 1;
+        nextIndex = currentIndex + 1;
     }
 
-    void GameOver()
+    IEnumerator stopMusic()
+    {
+        while (bgmSource.volume > 0)
+        {
+
+            bgmSource.volume = Mathf.Lerp(bgmSource.volume, 0f, Time.deltaTime);
+            yield return null;
+        }
+    }
+        void GameOver()
     {
         if(isGameDone) return;
+
+        if (bgmSource != null)
+        {
+            StartCoroutine(stopMusic());
+        }
+        if (gameDescriptorLabel)
+            gameDescriptorLabel.SetActive(false);
+        if (gameOverLabel)
+            gameOverLabel.SetActive(true);
 
         if (timerUI)
         {
@@ -436,6 +465,12 @@ public class GameManager : MonoBehaviour
     void Win()
     {
         if (isGameDone) return;
+        if (gameDescriptorLabel)
+            gameDescriptorLabel.SetActive(false);
+        if (gameClearLabel)
+            gameClearLabel.SetActive(true);
+        if (replayIcon)
+            replayIcon.SetActive(true);
         StartCoroutine(AnimateBubblesort());
         if (timerUI)
         {
@@ -460,8 +495,15 @@ public class GameManager : MonoBehaviour
 
     IEnumerator onGameComplete()
     {
-         
-        yield return new WaitForSeconds(6f);
+            
+        
+        yield return new WaitForSeconds(2f);
+        if (gameOverVideo)
+            gameOverVideo.SetActive(true);
+        yield return new WaitForSeconds(1.3f);
+            if(gameOverLabel)
+            gameOverLabel.SetActive(false);
+        yield return new WaitForSeconds(2.3f);
         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
     public void OnLeftClickInput(InputAction.CallbackContext context)
@@ -469,10 +511,10 @@ public class GameManager : MonoBehaviour
         if (context.performed)
         {
             OnLeftClick();
-            if(source && leftClip && rightClip)
+            if(fxSource && leftClip && rightClip)
             {
-                source.Stop();
-                source.PlayOneShot(leftClip);
+                fxSource.Stop();
+                fxSource.PlayOneShot(leftClip);
             }
         }
     }
@@ -481,10 +523,10 @@ public class GameManager : MonoBehaviour
     {
         if (context.performed)
         {
-            if (source && leftClip && rightClip)
+            if (fxSource && leftClip && rightClip)
             {
-                source.Stop();
-                source.PlayOneShot(rightClip);
+                fxSource.Stop();
+                fxSource.PlayOneShot(rightClip);
             }
             OnRightClick();
         }
